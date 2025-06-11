57472e94f7683612c60a1823e26a3c8877dd96c4
Filipe Silva <140545021+filipesilva9225@users.noreply.github.com>
2025-06-11 20:48:11 +0000

    add all files

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const nome = document.getElementById("nome");
+const mensagem = document.getElementById("mensagem");
+
+const telefone = "5575991681075";
+
+const texto = `
+    Olá! Me chamo ${nome}, estou enviando está mensagem: ${mensagem}
+`;
+
+const msgFormatada = mensagem;
