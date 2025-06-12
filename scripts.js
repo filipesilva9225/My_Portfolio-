a7a1b9bfe9b6863211da7cc2ddb917c4ec4feace
Filipe Silva <140545021+filipesilva9225@users.noreply.github.com>
2025-06-12 17:27:30 +0000

    add all files

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
-const nome = document.getElementById("nome");
-const mensagem = document.getElementById("mensagem");
+function enviarWhats(event) {
+  event.preventDefault();
 
-const telefone = "5575991681075";
+  const nome = document.getElementById("nome").value;
+  const mensagem = document.getElementById("mensagem").value;
 
-const texto = `
+  const telefone = "5575991681075";
+
+  const texto = `
     Olá! Me chamo ${nome}, estou enviando está mensagem: ${mensagem}
 `;
 
-const msgFormatada = mensagem;
+  const msgFormatada = encodeURIComponent(texto);
+
+  const url = `https://wame.me/${telefone}?text=${msgFormatada}`;
+
+  window.open(url, "-blank");
+}
